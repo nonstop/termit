0e648044aeb115503cafa3222523b37bbb6146dd
ratnikov.ev <ratnikov.ev@0cbafcd9-e234-0410-b4fb-b3f5331b89f7>
2009-06-23 18:56:06 +0000

    termit_preferences

--- src/Sources.cmake
+++ src/Sources.cmake
@@ -1,6 +1,7 @@
 SET( TERMIT_SRCS
    termit.c termit_core_api.c callbacks.c sessions.c
-   keybindings.c lua_conf.c lua_api.c configs.c termit_style.c
+   keybindings.c lua_conf.c lua_api.c configs.c
+   termit_style.c termit_preferences.c
 )
 SET( TERMIT_HDRS
     termit_core_api.h callbacks.h configs.h sessions.h
