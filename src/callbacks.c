3cb4fe752f36752333bf4dd2f626fd73a8e6dfa5
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2010-04-06 11:27:00 +0400

    added GPLv2 notice in every file

--- src/callbacks.c
+++ src/callbacks.c
@@ -1,3 +1,16 @@
+/*  Copyright (C) 2007-2010, Evgeny Ratnikov
+
+    This file is part of termit.
+    termit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 
+    as published by the Free Software Foundation.
+    termit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with termit. If not, see <http://www.gnu.org/licenses/>.*/
+
 #include <sys/wait.h>
 #include <stdint.h>
 #include <gdk/gdkkeysyms.h>
